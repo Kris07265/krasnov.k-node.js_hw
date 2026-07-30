4b4e993d81e1a56d827a4bb33407848e359a657d
Kris <krasnovkristian07@gmail.com>
2026-07-30 21:24:49 +0300

    Finished hw 20

---
+++ hw_20/getAverage.ts
@@ -0,0 +1,8 @@
+function getAverage(numbers: number[]) : number {
+    if (numbers.length === 0) {
+        return 0;
+    }
+
+    const sum = numbers.reduce((acc, val) => acc + val, 0);
+    return sum / numbers.length;
+}
